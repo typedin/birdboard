bcbf134114eb91c5ba991c8e3afcee558b3bb968
AntoineC <antoine@a-f.studio>
2020-04-22 17:02:33 +0200

    end of lesson 7

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::group(
     ["middleware" => "auth"],
     function () {
         Route::get("/projects", "ProjectsController@index");
+        Route::get("/project/create", "ProjectsController@create");
         Route::get("/projects/{project}", "ProjectsController@show");
         Route::post("/projects", "ProjectsController@store");
         Route::get('/home', 'HomeController@index')->name('home');
