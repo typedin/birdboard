15fae899e24ca99e24c263ca9b12f74a3b4fd6ed
AntoineC <antoine@a-f.studio>
2020-04-23 21:33:00 +0200

    end of lesson 15

--- app/Task.php
+++ app/Task.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $touches = ["project"];
     protected $fillable = ["body", "completed"];
 
     public function project()
