ec364173bfbba018aa9e46ac84d717b8ae7462d5
AntoineC <antoine@a-f.studio>
2020-05-02 13:04:50 +0200

    end of lesson 39

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+require("./bootstrap");
+import Vue from "vue";
+import Vmodal from "vue-js-modal";
+
+Vue.use(Vmodal);
+
+const app = new Vue({
+    el: "#app"
+});
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -15,4 +15,39 @@
             <div>No projects yet</div>
         @endforelse
     </main>
+    <modal name="hello-world"
+           classes="card p-10"
+           height="auto"
+    >
+        <h1 class="text-center text-2xl">Let's Start Something New!</h1>
+        <div class="mt-16 flex justify-between space-x-4">
+            <div class="flex-1">
+                <div>
+                   <label class="block text-sm" for="title">Title</label>
+                   <input class="form-input" type="text" name="title" placeholder="title"/>
+                </div>
+                <div class="mt-4">
+                   <label class="block text-sm" for="description">Description</label>
+                   <textarea class="form-input" name="description" placeholder="title" rows="7"></textarea>
+                </div>
+            </div>
+            <div class="flex-1">
+                <div>
+                   <label class="block text-sm">Need Some Tasks?</label>
+                   <input class="form-input" type="text" placeholder="task 1"/>
+                </div>
+                <button class="mt-4 inline-flex items-center text-xs">
+                    <svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 9V12M12 12V15M12 12H15M12 12H9M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                    <span class="ml-2">Add New Task Field</span>
+                </button>
+            </div>
+        </div>
+        <footer class="mt-4 flex justify-end space-x-4">
+            <button class="button is-outline">Cancel</button>
+            <button class="button">Create Project</button>
+        </footer>
+    </modal>
+    <button @click.prevent="$modal.show('hello-world')">Modal</button>
 @endsection
